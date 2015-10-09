c4973d7174a0dabe61b62287180ce3fb5c3fffe2
Erik Stevenson <eriknstevenson@gmail.com>
2015-10-09 00:45:43 -0400

    finished MakeMove action, refactored square data type.

--- tictactoe.elm
+++ tictactoe.elm
@@ -1,3 +1,5 @@
+module Tictactoe where
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -7,25 +9,39 @@ import StartApp.Simple as StartApp
 
 -- MODEL
 
-type Action = None
+type Action = None | MakeMove Int
 
-type alias Model = 
-  {
-    name : String,
-    board : Board
-  }
+type Player = Red | Blue
+
+toggleTurn : Player -> Player
+toggleTurn player = 
+  case player of
+    Red -> Blue
+    Blue -> Red
 
 type alias Board = List Square
 
-type Square = Empty | X | O
+type SquareStatus = Empty | X | O
+type alias Square = {status : SquareStatus, id : Int}
+
+newSquare : Square
+newSquare = 
+          {
+            status = Empty,
+            id = 0
+          }
+
+
+type alias Model = 
+  {
+    board : Board,
+    turn : Player
+  }
 
 initialModel : Model
-initialModel = 
-  {
-    name = "tictactoe",
-    board = [ Empty, Empty, X,
-              Empty, O,     Empty,
-              Empty, Empty, Empty ]
+initialModel = {
+    board = L.map2 (\sqr id -> {sqr | id <- id} ) (L.repeat 9 newSquare) [1..9],
+    turn = Red
   }
 
 --UPDATE
@@ -34,23 +50,33 @@ update : Action -> Model -> Model
 update action model =
   case action of
     None -> model
-
+    MakeMove id ->
+      let
+        newStatus = case model.turn of 
+          Red -> X
+          Blue -> O
+        updateStatus square = if square.id==id then {square | status <- newStatus} else square
+      in {model | board <- L.map updateStatus model.board,
+                  turn <- (toggleTurn model.turn)}
 -- VIEW
 
 view : S.Address Action -> Model -> Html
 view address model = 
-  let   
-    drawSquare : Square -> Html
-    drawSquare square = 
-      case square of
-        Empty -> text "-"
-        X -> text "x"
-        O -> text "o"
+  let
+    makeSquare : Square -> Html
+    makeSquare square =
+      case square.status of
+        Empty -> div [onClick address (MakeMove square.id), class "empty"] 
+          []
+        X -> div [class "x"]
+          [text "X"]
+        O -> div [class "o"]
+          [text "O"]
+      
 
   in
     div [] [
-      h1 [] [text model.name],
-      div [] (L.map drawSquare model.board)
+      div [id "board"] (L.map makeSquare model.board)
     ]
 
 main : Signal Html
